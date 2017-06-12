7539a2be2a824e6a8e952f7fff66da0336b116d7
nicolere <nicolas.gauss.1@orange.fr>
2017-06-12 23:53:40 +0200

    Ajout des Waypoint

--- RattrapageProject/src/main/java/carte/Carte.java
+++ RattrapageProject/src/main/java/carte/Carte.java
@@ -4,6 +4,9 @@ import java.awt.Component;
 import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.jxmapviewer.JXMapKit;
 import org.jxmapviewer.viewer.GeoPosition;
@@ -11,20 +14,36 @@ import org.jxmapviewer.viewer.GeoPosition;
 import image.ExtraireImage;
 import image.Image;
 import image.ManageImage;
+import waypoint.PainterModif;
+import waypoint.WaypointDefautModif;
 
 
 
 public class Carte {
 	private JXMapKit carte;
+	private Set<WaypointDefautModif> waypoint;
+	
+	
 	private Component clickonMap;
-	private ManageImage managImage;
+	private ManageImage manageImage;
 	private Image derniereimage;
+	private ArrayList<Image> ImageWaypointResultat;
+	private ArrayList<WaypointDefautModif> WaypointClicResultat;
+	
+	private PainterModif painter;
 
 
 public Carte() {
-	this.managImage = new ManageImage();
+	this.manageImage = new ManageImage();
 	this.carte = new JXMapKit();
 	this.carte.setDefaultProvider(JXMapKit.DefaultProviders.OpenStreetMaps);
+	
+	this.waypoint = new HashSet<WaypointDefautModif>();
+	this.WaypointClicResultat = new ArrayList<WaypointDefautModif>();
+	
+	this.painter = new PainterModif();
+	this.carte.getMainMap().setOverlayPainter(painter);
+	this.initialisation();
 	
 	
 }
@@ -34,22 +53,50 @@ public JXMapKit getMap() { //Obtention de la carte
 }
 
 public void paint(){
+	this.painter.setWaypoint(waypoint);
+	
 	this.carte.getMainMap().repaint();
 }
 
 public ManageImage getManageImage(){
-	return this.managImage;
+	return this.manageImage;
 }
 
 public ArrayList<Image> getImagList(){
-	return this.managImage.imagList;
+	return this.manageImage.imagList;
+}
+
+public ArrayList<WaypointDefautModif> RecupClicWaypointResultat(){
+	return this.WaypointClicResultat;
+}
+
+public ArrayList<Image> RecupImageWaypointResultat(){
+	this.ImageWaypointResultat = new ArrayList<Image>();
+	
+	for(int i=0;i<this.RecupClicWaypointResultat().size();i++){
+		int key = this.RecupClicWaypointResultat().get(i).getKey();
+		this.ImageWaypointResultat.add(this.getImagList().get(key));
+	}
+	return this.ImageWaypointResultat;
+}
+
+public void ajoutWaypoint(double coordx, double coordy, int key){
+	this.waypoint.add(new WaypointDefautModif(coordx,coordy,key));
+	this.paint();
+}
+
+public void ajoutWaypoint(GeoPosition point, int key){
+	this.waypoint.add(new WaypointDefautModif(point,key));
+	this.paint();
 }
 
 public void AjoutImage() throws IOException {
 	
 	Image newimag = ExtraireImage.LoadImage();
-	this.managImage.imagList.add(newimag);
+	this.manageImage.imagList.add(newimag);
 	this.derniereimage = newimag;
+	
+	this.ajoutWaypoint(newimag.Latitude,newimag.Longitude,this.manageImage.imagList.size()-1);
 }
 
 public Image getLastImag(){
@@ -60,6 +107,20 @@ public void centrerImag(GeoPosition pos){
 	this.carte.getMainMap().setCenterPosition(pos);
 	
 	this.paint();
+}
+
+public void initialisation() {
+	if(!this.manageImage.imagList.isEmpty()){
+		for(int i =0;i<this.manageImage.imagList.size();i++){
+			this.waypoint.add(new WaypointDefautModif(this.manageImage.imagList.get(i).Latitude,
+					this.manageImage.imagList.get(i).Longitude,i));
+		}
+	}
+	this.paint();
+}
+
+public Collection <? extends WaypointDefautModif> RecupSetWaypoint(){
+	return this.waypoint;
 }
 
 }
---
+++ RattrapageProject/src/main/java/waypoint/PainterModif.java
@@ -0,0 +1,13 @@
+package waypoint;
+
+import java.util.Set;
+
+import org.jxmapviewer.viewer.Waypoint;
+import org.jxmapviewer.viewer.WaypointPainter;
+public class PainterModif extends WaypointPainter<Waypoint> {
+	
+	public void setWaypoint(Set<? extends Waypoint> waypoints){
+		super.setWaypoints(waypoints);
+	}
+
+}
